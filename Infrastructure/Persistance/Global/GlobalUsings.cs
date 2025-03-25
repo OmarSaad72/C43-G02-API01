69b088343e6135a8af6cdc35276135ba682d3f3f
OmarSaad <OmarSaad@DESKTOP-2C0MESG>
2025-03-25 12:16:21 +0200

    Generic Repository

--- Infrastructure/Persistance/Global/GlobalUsings.cs
+++ Infrastructure/Persistance/Global/GlobalUsings.cs
@@ -7,3 +7,6 @@ global using System.Linq;
 global using System.Text;
 global using System.Threading.Tasks;
 global using System.Reflection;
+global using Domain.Contracts;
+global using Persistence.Data;
+
---
+++ Infrastructure/Persistance/Repositories/GenericRepository.cs
@@ -0,0 +1,28 @@
+﻿namespace Persistance.Repositories
+{
+    public class GenericRepository<TEntity, TKey> : IGenericRepository<TEntity, TKey> where TEntity : BaseEntity<TKey>
+    {
+        private readonly AppDbContext _dbContext;
+
+        public GenericRepository(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task Add(TEntity entity)
+            => await _dbContext.Set<TEntity>().AddAsync(entity);
+
+        public void Delete(TEntity entity)
+            => _dbContext.Set<TEntity>().Remove(entity);
+
+        public void Update(TEntity entity)
+            => _dbContext.Set<TEntity>().Update(entity);
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync(bool AsNoTracking)
+            => AsNoTracking ? await _dbContext.Set<TEntity>().AsNoTracking().ToListAsync() // True
+            : await _dbContext.Set<TEntity>().ToListAsync();  // False
+
+        public async Task<TEntity?> GetByIdAsync(TKey Id)
+            => await _dbContext.Set<TEntity>().FindAsync(Id);
+    }
+}
