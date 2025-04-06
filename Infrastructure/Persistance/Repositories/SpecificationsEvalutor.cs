d76e604c471e26a11b013dc4e0ebb9078330cd42
OmarSaad <OmarSaad@DESKTOP-2C0MESG>
2025-04-06 17:28:01 +0200

    SpecificationsEvalutor

---
+++ Infrastructure/Persistance/Repositories/SpecificationsEvalutor.cs
@@ -0,0 +1,17 @@
+﻿namespace Persistance.Repositories
+{
+    public static class SpecificationsEvalutor
+    {
+        public static IQueryable<T> GetQuery<T>(IQueryable<T> inputQuery, Specifications<T> specification) where T : class
+        {
+            var query = inputQuery;
+            if (specification.Criteria != null)
+                query = query.Where(specification.Criteria);
+            //foreach (var item in specification.IncludedExpressions)
+            //    query = query.Include(item);
+            query = specification.IncludedExpressions.Aggregate(query, (currentQuery, includeExpression)
+                => currentQuery.Include(includeExpression));
+            return query;
+        }
+    }
+}
