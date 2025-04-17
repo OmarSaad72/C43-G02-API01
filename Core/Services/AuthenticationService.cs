0698f3271645264b91fe720a984821342809697d
OmarSaad <OmarSaad@DESKTOP-2C0MESG>
2025-04-17 22:20:06 +0200

    Jwt, Create Token

--- Core/Services/AuthenticationService.cs
+++ Core/Services/AuthenticationService.cs
@@ -1,11 +1,14 @@
 ﻿using Domain.Entities;
 using Domain.Exceptions;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
 using Services.Abstraction;
 using Shared.DTOs;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -19,7 +22,7 @@ namespace Services
             if (user == null) throw new UnAuthorizedException();
             var result = await userManager.CheckPasswordAsync(user, loginDto.Password);
             if (result == false) throw new UnAuthorizedException();
-            return new UserResultDto(user.DisplayName, "Token", user.Email);
+            return new UserResultDto(user.DisplayName, await CreateTokenAsync(user), user.Email);
         }
 
         public async Task<UserResultDto> Register(RegisterDto registerDto)
@@ -37,7 +40,25 @@ namespace Services
                 var errors = result.Errors.Select(e => e.Description).ToList();
                 throw new ValidationException(errors);
             }
-            return new UserResultDto(user.DisplayName, "Token", user.Email);
+            return new UserResultDto(user.DisplayName, await CreateTokenAsync(user), user.Email);
+        }
+        private async Task<string> CreateTokenAsync(User user)
+        {
+            // Private Claims
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.DisplayName),
+                new Claim(ClaimTypes.Email, user.Email),
+            };
+            var roles = await userManager.GetRolesAsync(user);
+            foreach (var role in roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("5758868757ruyrfdyurf763rkfndksnf76rdfy5e54e6765r8779t79tg78tgjyfu6yr8500y90yh987ew451q34rf67rfif787f6"));
+            var signingCreds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            var token = new JwtSecurityToken(issuer: "https://localhost:7224/", audience: "UrlFrontEnd", claims, DateTime.UtcNow.AddDays(15), signingCredentials: signingCreds);
+        return new JwtSecurityTokenHandler().WriteToken(token);
         }
     }
 }
