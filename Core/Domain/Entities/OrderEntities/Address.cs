6ac2c79ca99c031ced407ae31225ba2756b2432b
OmarSaad <OmarSaad@DESKTOP-2C0MESG>
2025-04-18 18:39:22 +0200

    Order Module

---
+++ Core/Domain/Entities/OrderEntities/Address.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Entities.OrderEntities
+{
+    public class Address
+    {
+        public string FName { get; set; }
+        public string LName { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+    }
+}
---
+++ Core/Domain/Entities/OrderEntities/Order.cs
@@ -0,0 +1,15 @@
+﻿namespace Domain.Entities.OrderEntities
+{
+    public class Order : BaseEntity<Guid>
+    {
+        public string UserEmail { get; set; }
+        public Entities.OrderEntities.Address ShippingAddress { get; set; }
+        public IEnumerable<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
+        public OrderPaymentStatus PaymentStatus { get; set; } = OrderPaymentStatus.Pending;
+        public DeliveryMethods DeliveryMethods { get; set; }
+        public int? DeliveryMethodsId { get; set; }
+        public decimal Subtotal { get; set; }
+        public DateTimeOffset OrderDate { get; set; } = DateTimeOffset.Now;
+        public string PaymentIntentId { get; set; } = string.Empty;
+    }
+}
